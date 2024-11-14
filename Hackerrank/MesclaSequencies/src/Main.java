0bfe4c71e347c03d79c1875b68a569286665ea08
pauolt <pauolt2@alu.edu.gva.es>
2024-11-14 13:54:04 +0100

    HE hecho mezcla de sequencias y ahora estoy haciendo el de par impar

--- Hackerrank/MesclaSequencies/src/Main.java
+++ Hackerrank/MesclaSequencies/src/Main.java
@@ -12,23 +12,23 @@ public class Main {
         }
 
         int n2 = scanner.nextInt();
-        int[] array2 = new int[n1];
+        int[] array2 = new int[n2];
 
         for (int i = 0; i < n2; i++){
             array2[i] = scanner.nextInt();
         }
 
-        int tam = n1+n2;
-        int[] array3 = new int[tam];
-
-        for (int i = 0; i < n1; i++){
-            array3[i] = array1[i];
-            array3[i+1] = array2[i];
+        int masLargo = array1.length;
+        if(array2.length > masLargo){
+            masLargo = array2.length;
         }
-
-        for (int i = 0; i < array3.length; i++){
-            System.out.println(array3[i]);
+        for (int i = 0; i < masLargo; i++){
+            if(i < array1.length){
+                System.out.print(array1[i] + " ");
+            }
+            if(i < array2.length){
+                System.out.print( array2[i] + " ");
+            }
         }
-
     }
 }
