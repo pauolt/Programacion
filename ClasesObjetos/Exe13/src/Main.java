0fce48921b352a6a842d7fceb5ae23cf4738441c
pauolt2 <pauolt2@alu.edu.gva.es>
2025-02-04 13:52:27 +0100

    .

--- ClasesObjetos/Exe13/src/Main.java
+++ ClasesObjetos/Exe13/src/Main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Random;
 import java.util.Scanner;
 
 class Player {
-    boolean [] catGuessed = {false, false , false};
+    boolean[] categories = new boolean[6];
 
 }
 
@@ -15,6 +16,7 @@ class Question {
     String C;
     String D;
     String corr;
+    boolean wasAsked = false;
 
     Question(String category, String question, String A, String B, String C, String D, String corr){
         this.category = category;
@@ -25,17 +27,22 @@ class Question {
         this.D = D;
         this.corr = corr;
 
+
     }
 
-    String getInfo (){
-        return category + "\n" + question + "\n" + A + "\n" + B + "\n" + C + "\n" + D;
+    String getQuestion (){
+        return category + "\n" + question + "\nA." + A + "\nB." + B + "\nC." + C + "\nD." + D;
     }
+
+
+
 }
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         Scanner fichero = new Scanner(Path.of("/home/pauolt2/IdeaProjects/Programacion/preguntas.txt"));
+        Random random = new Random();
 
         System.out.println("Cuantos sois?");
 
@@ -48,8 +55,10 @@ public class Main {
                 players[i] = new Player();
         }
 
-        Question[] questions = new Question[fichero.nextInt()];
+        int nQuestions = fichero.nextInt();
         fichero.nextLine();
+        Question[] questions = new Question[nQuestions];
+
 
         for (int i = 0; i < questions.length; i++) {
             String question = fichero.nextLine();
@@ -63,8 +72,40 @@ public class Main {
             questions[i] = new Question(category,question,a,b,c,d,correct);
         }
 
-        for (int i = 0; i < questions.length; i++){
-            System.out.println(questions[i].getInfo());
+        int player = 0;
+        while(true){
+            System.out.println("Jugador" + (player + 1));
+
+            int nQuestion;
+            while(true) {
+                nQuestion = random.nextInt(nQuestions);
+                if (!questions[nQuestion].wasAsked){
+                    break;
+                }
+            }
+            System.out.println(questions[nQuestion].getQuestion());
+            String answer = scanner.nextLine();
+
+            if (answer.equals(questions[nQuestion].corr)){
+                System.out.println("CORRECTO!");
+                System.out.println("Categoria " + questions[nQuestion].category + " superada!");
+
+                int nCategory = switch (questions[nQuestion].category) {
+                    case "Historia" -> 1;
+                    case "Literatura" -> 2;
+                    case "Deportes" -> 3;
+                    case "Geografia" -> 4;
+                    case "Arte" -> 5;
+                    default -> 0;
+                };
+                players[player].categories[nCategory] = true;
+
+            } else {
+                System.out.println("Has fallado.");
+            }
+
+            questions[nQuestion].wasAsked = true;
+            player = (player + 1) % n;
 
         }
 
