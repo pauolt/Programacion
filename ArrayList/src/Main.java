e419a9aa1b88a292a1c2e354066ef12fe719ebe7
pauolt2 <pauolt2@alu.edu.gva.es>
2025-03-12 12:03:40 +0100

    ...

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+record Alumno(String nombre){}
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Alumno> listaAlumnos = new ArrayList<>();
+        listaAlumnos.add(new Alumno("Paco"));
+        listaAlumnos.add(new Alumno("Pepe"));
+        listaAlumnos.addFirst(new Alumno("Juan"));
+
+        System.out.println( listaAlumnos.size());
+        System.out.println( listaAlumnos.isEmpty());
+
+        Alumno retornado = listaAlumnos.set(1, new Alumno("Pau"));
+        System.out.println(retornado);
+
+        System.out.println();
+        for (Alumno alumno: listaAlumnos){
+            System.out.println(alumno.nombre());
+        }
+
+
+    }
+}
